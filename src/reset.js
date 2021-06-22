4d5db6cebce356c2e367a68e809a9bcd0cb0658f
Conrad Magnus Kirschner <s0566895@htw-berlin.de>
2021-06-22 20:31:40 +0200

    ....

--- src/reset.js
+++ src/reset.js
@@ -1,6 +1,6 @@
-const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-const RESET = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
-RESET_gpio(); //run the blinkRESET function every 250ms
+// const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+// const RESET = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+// RESET_gpio(); //run the blinkRESET function every 250ms
 
 export function RESET_gpio() { //function to start blinking
     RESET.writeSync(1); //set pin state to 1 (turn RESET on)
@@ -9,6 +9,6 @@ export function RESET_gpio() { //function to start blinking
 
 function endBlink() { //function to stop blinking
     RESET.writeSync(0); // Turn RESET off
-    // RESET.unexport(); // Unexport GPIO to free resources
+    RESET.unexport(); // Unexport GPIO to free resources
 }
 
