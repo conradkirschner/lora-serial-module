0ec1bee2028a0c1f1f7fa9f929d0333b65d4aedf
Conrad Magnus Kirschner <s0566895@htw-berlin.de>
2021-06-23 18:02:29 +0200

    ....

--- src/test-app.js
+++ src/test-app.js
@@ -12,12 +12,13 @@ port.on('error', function (err) {
 
 // Read data that is available but keep the stream in "paused mode"
 port.on('readable', function () {
-    console.log('Data:', port.read())
+    const result =  port.read();
+    console.log('Data - stopped:', result, result.toString())
 })
 
 // Switches the port into "flowing mode"
 port.on('data', function (data) {
-    console.log('Data:', data)
+    console.log('Data - flow mode:', data, data.toString())
 })
 
 port.pipe(new SerialPort.parsers.Readline({ delimiter: `\r\n`, encoding:'ascii' }))
