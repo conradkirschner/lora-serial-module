567bb56159240e2132757a8135f227e1e297732e
Conrad Magnus Kirschner <s0566895@htw-berlin.de>
2021-06-23 20:25:08 +0200

    ....

--- src/client/packages/RREQ.js
+++ src/client/packages/RREQ.js
@@ -1,14 +1,14 @@
 import {RREQ} from "./types";
 
 export const create = (uflag, hopCount, originAddress, rreq_id, originSequenceNumber, destinationAddress, destinationSequenceNumber) => {
-    return Buffer.from([RREQ, (uflag)?uflag:1, hopCount, originAddress, rreq_id, originSequenceNumber, destinationAddress, destinationSequenceNumber]);
+    return Buffer.from([RREQ, (uflag)?uflag:1, hopCount, rreq_id, originAddress,  originSequenceNumber, destinationAddress, destinationSequenceNumber]);
 }
 export const read = (byteArray) => {
     return {
         uflag: byteArray[0],
         hopCount: byteArray[1],
-        originAddress: byteArray[2],
-        rreq_id: byteArray[3],
+        rreq_id: byteArray[2],
+        originAddress: byteArray[3],
         originSequenceNumber: byteArray[4],
         destinationAddress: byteArray[5],
         destinationSequenceNumber: byteArray[6],
