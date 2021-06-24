0f1606c6416dfe8819e4023773069d950e774d7a
Conrad Magnus Kirschner <s0566895@htw-berlin.de>
2021-06-24 10:53:58 +0200

    ....

--- proxy/client-test.js
+++ proxy/client-test.js
@@ -4,6 +4,7 @@ const ws = new WebSocket('ws://localhost:8080');
 
 ws.on('open', function open() {
     console.log('connected');
+    ws.send('AT+RST\r\n');
 });
 
 ws.on('message', function incoming(data) {
